569876ea68893ba1403f80dbf6e50145a4465ba7
lonelyyhu <lonelyyhu@noodoe.com>
2019-01-10 17:47:13 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const stripe = require("stripe")("sk_test_Ng7u88sF7itap0aIDhjR3plp")
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.post('/card/:cardToken', async (req, res) => {
+
+    const customer = await stripe.customers.create({
+        source: req.params.cardToken,
+        email: 'paying.user@example.com'
+    })
+
+    console.log(customer)
+
+    res.send('success!')
+})
+
+app.post('/customer/:customerId/addCard/:cardToken', async (req, res) => {
+
+    const { customerId, cardToken } = req.params
+
+    const newCard = await stripe.customers.createSource(
+        customerId,
+        { source: cardToken }
+    )
+
+    console.log(newCard)
+
+    res.send('success!')
+})
+
+app.get('/customer/:customerId/cards', async (req, res) => {
+
+    const { customerId } = req.params
+
+    const cardList = await stripe.customers.listCards(customerId)
+
+    console.log(cardList)
+
+    res.send('success!')
+})
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!')
+})
